7d0f0b8c8f9e66452b14be8797206e14b355cc4a
Raunak Redkar <raunak.redkar@gmail.com>
2023-09-21 20:59:41 +0200

    roughly finished, NO castle enpass prom tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub enum PieceType {
     King,
 }
 
+
 pub enum Color { 
     Black,
     White,
@@ -18,6 +19,12 @@ pub enum Color {
 pub struct Piece {
     piece: PieceType, 
     color: Color, 
+}
+
+pub struct Decision {
+    White,
+    Black,
+    Tie,
 }
 
 /*
@@ -96,12 +103,22 @@ impl Game {
         }
     }
     /* should perform a move if possible */
-    pub fn do_move(&mut self, mv: Move) {
+    pub fn do_move(&mut self, mv: Move) -> result::Result<Option<Decision>, MoveError> {
 
         // if found enemy pieces, means king is still checked, and must undo move
 
-        if !legal_movement(mv) {
+        /*
+        if !legal_movement(mv.clone()) {
            // return some error  
+        }
+        */
+        match legal_movement(mv) { 
+            Some(x) => {
+                return Err(x);
+            }
+            None => {
+                // just a normal move
+            }
         }
 
         let saved_start: Option<Piece> = self.board[mv.start_y][mv.start_x].clone();
@@ -110,6 +127,7 @@ impl Game {
 
         // potentially temporarily make the move
         self.board[mv.start_y][mv.start_x] = Option<Piece>::None;
+        // BIG Todo! : the_piece is not in scope
         self.board[mv.end_y][mv.end_x] = the_piece;
 
         if inCheck(self.turn) { 
@@ -121,7 +139,6 @@ impl Game {
         // else continue to mate check
         let safe_move: bool = false;
 
-        // todo! : check if position is a mate
         // i'm sure there is a better way or writing this mate check. The complexity is through the
         // roof here.
         'move_gen: for org_Y in 1..8 {
@@ -140,9 +157,23 @@ impl Game {
         if !safe_move {
             // self.turn has won
             // signal end of game or something
+            let winner = match self.turn {
+                Color::White => "White",
+                Color::Black => "Black",
+            };
+            let loser = match self.turn {
+                Color::White => "Black",
+                Color::Black => "White",
+            };
+            let decision = match self.turn { 
+                Color::White => Decision::White,
+                Color::Black => Decision::Black,
+            };
+            println!("{} has checkmated {}, and won the game", winner, loser);
+            return Ok(Some(decision));
         }
         else {
-            if saved_start.unwrap() == PieceType::Pawn {
+            if saved_start.unwrap().piece == PieceType::Pawn {
                 self.move_history.push(MoveType::capture_or_pawn);
             }
             else {
@@ -156,28 +187,46 @@ impl Game {
                 }
             }
         }
+        // check for 50 move draw rule, and force draw like in chess com
+        let pawn_capture_move: bool = false;
+        for i in (self.move_history.len()-50..self.move_history.len()).rev() {
+            if self.move_history[i as usize] == MoveType::capture_or_pawn {
+                pawn_capture_move = true;
+                break;
+            }
+        }
+        // todo! check all let matches for semicolon
+        if !pawn_capture_move {
+            // the game is drawn
+            // end game or something
+            // dont know println is the best way to handle this
+            println!("The game is drawn because 50 reversible moves have been played");
+            return Ok(Some(Decision::Tie));
+        }
+        return Ok(None);
     }
 
     // checks for move legality
-    pub fn legal_movement(brd: Board, mv: Move) -> result::Result<Option<Color>, MoveError>> {
+    pub fn legal_movement(brd: Board, mv: Move /* need to pass the_piece and endsquare by reference here */) -> Option<MoveError> {
         /* check possible mv errors in order */
+        //todo! come back here
         let board_y = 0..8;
         let board_x = 0..8;
         // if the position doesnt change
         if mv.start_x == mv.end_x && mv.start_y == mv.end_y { 
-            return Err(MoveError::movement);
+            return Some(MoveError::movement);
         }
         // if the moves is in bounds
         if !board_x.contains(mv.start_x) || !board_x.contains(mv.end_x) || !board_y.contains(mv.start_y) || !board_y.contains(mv.end_y) {
-            return Err(MoveError::outsideBoard);
+            return Some(MoveError::outsideBoard);
         }
         let the_piece = match self.board[mv.start_y][mv.start_x] {
-            None => return Err(MoveError::noPiece),
+            None => return Some(MoveError::noPiece),
             Some(p) => if p.color != self.turn {
-                return Err(MoveError::wrongColorPiece)
+                return Some(MoveError::wrongColorPiece)
             }
             else {
-                Some(p);
+                Some(p),
             }, 
         };
         
@@ -186,14 +235,14 @@ impl Game {
         let end_square = match self.board[mv.start_y][mv.start_x] {
             None => Option::<Piece>::None, 
             Some(p) => if p.color == self.turn {
-                return Err(MoveError::wrongColorPiece)
+                return Some(MoveError::wrongColorPiece)
             }
             else {
                 capture = 1;
-                Some(p);
+                Some(p),
             }, 
         };
-
+        // todo! the_piece and end_square are of type Option<Piece>
         let dx: usize = { 
             if mv.end_x - mv.start_x > 0 {1}
             else if mv.end_x - mv.start_x < 0 {-1}
@@ -210,92 +259,83 @@ impl Game {
             // check if it right type of move 
             PieceType::Pawn => {
                 if abs(mv.end_x - mv.start_x) != { if capture {1} else {0} } {
-                    return Err(MoveError::movement);
+                    return Some(MoveError::movement);
                 }
                 let yDif = mv.end_y - mv.start_y;
                 if mv.start_y == 0 || mv.start_y == 7 {
                     if yDif > { if self.turn == White {2} else {-2} } {
-                        return Err(MoveError::movement);
+                        return Some(MoveError::movement);
                     }
                 }
                 else {
                     if yDif > { if self.turn == White {1} else {-1} } {
-                        return Err(MoveError::movement);
+                        return Some(MoveError::movement);
                     }
                 }
-                // else do the move;
             }
             PieceType::Knight => {
                 // should always have legal movement at this stage
             }
             PieceType::Bishop => {
                 if abs(mv.end_y - mv.start_y) != abs(mv.end_x - mv.start_x) {
-                    return Err(MoveError::movement);
+                    return Some(MoveError::movement);
                 }
 
                 let curX = mv.start_x;
                 let curY = mv.start_y;
                 while (curX != mv.end_x) || (curY != mv.end_y) {
                     if  (curX != mv.start_x) || (curY != mv.start_y) {
-                        if self.board[curY][curX] != Option<Piece>::None {
-                            return Err(MoveError::blockedPath);
+                        if self.board[curY][curX].is_some() {
+                            return Some(MoveError::blockedPath);
                         }
                     }
                     curX += dx;
                     curX += dy;
                 }
 
-                //self.board[mv.start_y][mv.start_x] = Option<Piece>::None;
-                //self.board[mv.end_y][mv.end_x] = the_piece;
             }
             PieceType::Rook => {
                 if mv.end_y - mv.start_y != 0 && mv.end_x - mv.end_x != 0 { 
-                    return Err(MoveError::movement);
+                    return Some(MoveError::movement);
                 }
 
                 let curX = mv.start_x;
                 let curY = mv.start_y;
                 while (curX != mv.end_x) || (curY != mv.end_y) {
                     if  (curX != mv.start_x) || (curY != mv.start_y) {
-                        if self.board[curY][curX] != Option<Piece>::None {
-                            return Err(MoveError::blockedPath);
+                        if self.board[curY][curX].is_some() {
+                            return Some(MoveError::blockedPath);
                         }
                     }
                     curX += dx;
                     curX += dy;
                 }
-                //self.board[mv.start_y][mv.start_x] = Option<Piece>::None;
-                //self.board[mv.end_y][mv.end_x] = the_piece;
             }
             PieceType::Queen => {
-                /* todo! : need to check if there is something in between */
                 let Y = abs(mv.end_y - mv.start_y);
                 let X = abs(mv.end_x - mv.start_x);
                 if (cmp::max(X, Y) != cmp::min(X, Y) && cmp::min(X, Y) != 0) {
-                    return Err(MoveError::movement);
+                    return Some(MoveError::movement);
                 }
                 let curX = mv.start_x;
                 let curY = mv.start_y;
                 while (curX != mv.end_x) || (curY != mv.end_y) {
                     if  (curX != mv.start_x) || (curY != mv.start_y) {
-                        if self.board[curY][curX] != Option<Piece>::None {
-                            return Err(MoveError::blockedPath);
+                        if self.board[curY][curX].is_some() {
+                            return Some(MoveError::blockedPath);
                         }
                     }
                     curX += dx;
                     curX += dy;
                 }
-                //self.board[mv.start_y][mv.start_x] = Option<Piece>::None;
-                //self.board[mv.end_y][mv.end_x] = the_piece;
             }
             PieceType::King => {
                 // check if the attempted move is dx = 2 (potential attempt to castle)
                 // otherwise there should be no problem
-                //self.board[mv.start_y][mv.start_x] = Option<Piece>::None;
-                //self.board[mv.end_y][mv.end_x] = the_piece;
             }
         }
         
+        return None;
     }
 
     pub fn inCheck(&mut self, C: Color) -> bool {
@@ -343,6 +383,6 @@ impl Game {
                 break 'outer;
             }
         }
-    }
+        return checked;
 
 }
